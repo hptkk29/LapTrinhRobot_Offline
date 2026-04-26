210ec5ed5d6a298c728a9e31a60903e84e448c37
hptkk29 <satarobo@gmail.com>
2026-04-26 09:59:18 +0700

    Refactor offline landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import OfflinePage from './pages/OfflinePage'
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<OfflinePage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <OfflinePage />
 }
